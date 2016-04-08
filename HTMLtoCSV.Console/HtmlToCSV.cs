5d8417f95d329017c72286d55da2800a8de2cc97
jeffward01 <jeffwardcoding@gmail.com>
2016-04-08 12:58:49 -0700

    added save file feature

--- HTMLtoCSV.Console/HtmlToCSV.cs
+++ HTMLtoCSV.Console/HtmlToCSV.cs
@@ -11,7 +11,7 @@ namespace HTMLtoCSV.Console
     {
        public static string GetCSVAttachement()
        {
-           //Get XML Values (IE: Html in a strong format)
+           //Get XML Values (IE: Html in a string format)
            var XMLstring =
                "<h2>Advertising License Request</h2><center><table border='1' cellpadding='3'><tr style='background-color:#EEE;'><td style='width:25%'>Full Name</td><td>sdcsaca</td></tr><tr><td style='width:25%'>Company Name</td><td>asc</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Full Address</td><td>asc, </td></tr><tr><td style='width:25%'>Country</td><td>United States</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Phone</td><td>4159480160</td></tr><tr><td style='width:25%'>Fax</td><td>No Fax number listed</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Email</td><td>sacsa@gmail.com</td></tr><tr><td style='width:25%'>Publishing Company</td><td></td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Artist/Master/Album(if you are using Master)</td><td>Master Used: True</td></tr><tr><td style='width:25%'>Have you cleared the Master? ('Master' refers to the master recording, the copyrighted performance of the artist or band who made the song popular, or a copyrighted performance by a cover band. the master must be licensed from a record label.)</td><td>Master Cleared: True</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Project Title</td><td>324324</td></tr><tr><td style='width:25%'>Budget</td><td>234</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Music Budget</td><td>234</td></tr><tr><td style='width:25%'>Release/Air Date(Film must be released no later than 6 months from submission date of request) </td><td>3/17/2016 7:00:00 AM</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Scene Description</td><td>234324</td></tr><tr><td style='width:25%'>Total Length of Song Use</td><td>Duration: 234</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Number of Songs in Film</td><td>342</td></tr><tr><td style='width:25%'>Will you be altering the lyrics in any way? </td><td>Lyrics Changed: Parody Lyrics: True</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Media </td><td></td></tr><tr><td style='width:25%'>Term </td><td>324234</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Territory </td><td>324</td></tr><tr><td style='width:25%'>Web Address </td><td>google.com</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Proposed Structure </td><td>234</td></tr><tr><td style='width:25%'>Timing Type ID </td><td>3</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Paid Media Online </td><td>432</td></tr><tr><td style='width:25%'>Media Rights</td><td>True</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Re-Record</td><td>True</td></tr><tr><td style='width:25%'>Renewal for Second Term</td><td>True</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Extended to new territory</td><td>True</td></tr><tr><td style='width:25%'>Add Commercials to Campaign</td><td>True</td></tr><tr style='background-color:#EEE;'><td style='width:25%'>Lyrics</td><td>324324324324234234234</td></tr></table></center><br/><h3>Requested Songs</h3><table style='width:50%; border:solid 1px #666;'><tr><td style='width:10%; font-weight:bold;'>#</td><td style='font-weight:bold'>Song Title</td><td style='font-weight:bold'>Song Artist</td><td style='font-weight:bold'>Song Writer</td></tr><tr><td>1). </td><td>234324</td><td>234324</td><td>234234</td></tr><tr><td>2). </td><td>234324</td><td>23423</td><td>234234</td></tr><tr><td>3). </td><td>234234</td><td>234324</td><td>234324</td></tr><tr><td>4). </td><td>23432</td><td>234234</td><td>234234</td></tr></table><br/>";
 
@@ -65,11 +65,13 @@ namespace HTMLtoCSV.Console
            {
                //build string to hold values
                StringBuilder songValues = new StringBuilder();
-               var counter = 1;
+               var songCount = node.Descendants("td").Count();
+               var counter = 0;
+               songCount = songCount - 1;
                foreach (HtmlNode node2 in node.Descendants("td"))
                {
                    var attribute = node2.InnerHtml;
-                   if (counter < 4)
+                   if (counter < songCount)
                    {
                        songValues.Append(attribute + ",");
                    }
--- HTMLtoCSV.Console/Program.cs
+++ HTMLtoCSV.Console/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +15,24 @@ namespace HTMLtoCSV.Console
         {
             System.Console.WriteLine("TEST");
             var output = HtmlToCSV.GetCSVAttachement();
+
+            
+            var path = String.Format("{0}csvFiles\\", AppDomain.CurrentDomain.BaseDirectory);
+            var filePath = System.IO.Path.Combine(path, "LicenseRequest.csv");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            File.WriteAllText(filePath, output);
+            
+            
+
+
+
             System.Console.WriteLine(output);
+            System.Console.WriteLine(filePath);
             System.Console.ReadLine();
 
         }
